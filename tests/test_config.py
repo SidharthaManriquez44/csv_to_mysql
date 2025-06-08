6c160568416bebc73be686e96c39db286b07ec48
SidharthaManriquez44 <sidhmanr02@gmail.com>
2025-06-07 19:31:16 -0600

    test: Add test validation when try SQL injection

--- tests/test_config.py
+++ tests/test_config.py
@@ -18,3 +18,22 @@ def test_get_engine(mocker):
     mock_connect.assert_called_once()
 
     assert conn == mock_engine
+
+
+def test_get_engine_is_not_safe(mocker):
+    # created the engine mock
+    mock_engine = mocker.MagicMock()
+    mock_engine.__enter__.return_value = mock_engine
+    mock_engine.__exit__.return_value = None
+
+    # Patch sqlalchemy.create_engine to return mock_engine
+    mock_connect =mocker.patch("src.config.create_engine", return_value=mock_engine)
+
+    # Catching SQL injection, the code that uses sqlalchemy.create_engine with sql injection
+    with pytest.raises(ValueError, match="Invalid or suspicious value"):
+        connection = MySQLConnection(user="user", password="select * from users", host="localhost", port="3306",
+                                     db_name="test")
+        connection.get_engine()
+
+    # Verify that create_engine was called and that it returns the correct mock
+    mock_connect.assert_not_called()
